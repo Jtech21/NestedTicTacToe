250b4d9144581d9fc739cbc8de00b44ff1eb78c1
Jake Graham <jake.graham.95@gmail.com>
2020-06-14 00:58:19 -0400

    connected gridspaces to boardspaces, connected boardspaces to gamecontroller

--- NestedTicTacToe/Assets/Scripts/BoardSpace.cs
+++ NestedTicTacToe/Assets/Scripts/BoardSpace.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoardSpace : MonoBehaviour
 {
     private GameController gameController;
+    public Button[] buttonlist;
+    
 
     public void SetGameControllerReferance(GameController controller)
     {
